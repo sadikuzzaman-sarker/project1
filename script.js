31e55919e80a8c70f6f06fb8a8b8ffdd161a9d0e
sadikuzzaman-sarker <sadikuzzamansarker8@gmail.com>
2025-12-31 11:26:09 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// future animation / interaction will go here
+function revealOnScroll() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const elementVisible = 100;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealOnScroll);
